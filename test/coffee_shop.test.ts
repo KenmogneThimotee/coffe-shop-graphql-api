dd0e4b49198287e0f4153938a93c39c9a508d172
Kenmogne Thimotee <kenmognethimotee@gmail.com>
2022-12-07 18:27:20 +0100

    Initial commit

---
+++ test/coffee_shop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CoffeeShop from '../lib/coffee_shop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/coffee_shop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CoffeeShop.CoffeeShopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
